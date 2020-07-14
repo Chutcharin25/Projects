56871caab724e4fd788e1d2e7661a0d77a6b9073
Wattana <tuen1997@gmail.com>
2020-07-14 09:52:57 +0700

    WEB

--- WATTANA.php
+++ WATTANA.php
@@ -1,1 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>HOME</title>
+
+    <style type="text/css">
+body {
+  background: url("https://www.shutterstock.com/th/blog/wp-content/uploads/sites/16/2018/09/13.jpg?w=750") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  
+}
+</style>
+
+  </head>
+  <body>
+  <form>
+      <label for="usr"><font style="position:relative; right:-1000px; top:390px;"><h5>Username :</h5></label>
+      <input type="text" id="usr"></font>
+      <br>
+      <label for="pwd"><font style="position:relative; right:-1005px ; top:400px;"><h5>Password :</h5></label>
+      <input type="password" id="pwd"></font>
+  </form>
+  <button type="button" class="btn btn-info" style="position:relative; right:-1080px ; top:410px;">เข้าสู่ระบบ</button>
+  <button type="button" class="btn btn-info" style="position:relative; right:-1080px ; top:410px;">สมัครสมาชิค</button>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
